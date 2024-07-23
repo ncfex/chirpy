28d9ccec24422688e35a10c526ada728b1817c88
ncfex <oguzhanguldibi@gmail.com>
2024-07-23 23:16:20 +0300

    testing

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ncfex/chirpy
+
+go 1.22.5
---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	const filepathRoot = "."
+	const port = "8080"
+
+	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(srv.ListenAndServe())
+}
