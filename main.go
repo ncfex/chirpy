e7e7da505521686f95105f86bdea7ca23fad6f09
ncfex <oguzhanguldibi@gmail.com>
2024-07-23 23:50:55 +0300

    added health checker endpoint

--- main.go
+++ main.go
@@ -10,7 +10,8 @@ func main() {
 	const port = "8080"
 
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	mux.Handle("/app/*", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.HandleFunc("/healthz", readiness)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
@@ -19,4 +20,14 @@ func main() {
 
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(srv.ListenAndServe())
+}
+
+func readiness(rw http.ResponseWriter, r *http.Request){
+	rw.WriteHeader(http.StatusOK)
+	rw.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	responseText := "OK"
+	_, err := rw.Write([]byte(responseText))
+	if err != nil {
+		log.Fatalf("Server can't set response body!")
+	}
 }
