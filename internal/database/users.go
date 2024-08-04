b1296fdcda5f1b84e7af000a28d345f3d7507e45
ncfex <oguzhanguldibi@gmail.com>
2024-08-04 21:21:45 +0300

    Separated users

---
+++ internal/database/users.go
@@ -0,0 +1,111 @@
+package database
+
+import (
+	"errors"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+var ErrAlreadyExists = errors.New("already exists")
+
+type User struct {
+	Id       int    `json:"id"`
+	Email    string `json:"email"`
+	Password []byte `json:"password"`
+}
+
+func (db *DB) CreateUser(email string, hashedPassword []byte) (User, error) {
+	if _, err := db.GetUserByEmail(email); !errors.Is(err, ErrNotExist) {
+		return User{}, ErrAlreadyExists
+	}
+
+	dbStructure, err := db.loadDB()
+	if err != nil {
+		return User{}, err
+	}
+
+	id := len(dbStructure.Users) + 1
+	newUser := User{
+		Id:       id,
+		Email:    email,
+		Password: hashedPassword,
+	}
+
+	dbStructure.Users[id] = newUser
+
+	if err = db.writeDB(dbStructure); err != nil {
+		return User{}, err
+	}
+
+	return newUser, nil
+}
+
+func (db *DB) GetUsers() ([]User, error) {
+	dbStructure, err := db.loadDB()
+	if err != nil {
+		return nil, err
+	}
+
+	users := make([]User, 0, len(dbStructure.Users))
+	for _, user := range dbStructure.Users {
+		users = append(users, user)
+	}
+
+	return users, nil
+}
+
+func (db *DB) GetUser(id int) (User, error) {
+	dbStructure, err := db.loadDB()
+	if err != nil {
+		return User{}, err
+	}
+
+	user, ok := dbStructure.Users[id]
+	if !ok {
+		return User{}, ErrNotExist
+	}
+
+	return user, nil
+}
+
+func (db *DB) GetUserByEmail(email string) (User, error) {
+	dbStructure, err := db.loadDB()
+	if err != nil {
+		return User{}, err
+	}
+
+	for _, user := range dbStructure.Users {
+		if user.Email == email {
+			return user, nil
+		}
+	}
+
+	return User{}, ErrNotExist
+}
+
+func (db *DB) UpdateUser(id int, email string, hashedPassword []byte) (User, error) {
+	dbStructure, err := db.loadDB()
+	if err != nil {
+		return User{}, err
+	}
+
+	user, ok := dbStructure.Users[id]
+	if !ok {
+		return User{}, ErrNotExist
+	}
+
+	newHashedPw, err := bcrypt.GenerateFromPassword(hashedPassword, 10)
+	if err != nil {
+		return User{}, err
+	}
+
+	user.Password = newHashedPw
+	user.Email = email
+	dbStructure.Users[id] = user
+
+	if err = db.writeDB(dbStructure); err != nil {
+		return User{}, err
+	}
+
+	return user, nil
+}
