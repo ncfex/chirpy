e6f1939e698fe166aa59f8a49441727307b8d96a
ncfex <oguzhanguldibi@gmail.com>
2024-08-04 21:06:35 +0300

    Separated chirps

---
+++ internal/database/chirps.go
@@ -0,0 +1,55 @@
+package database
+
+import "errors"
+
+type Chirp struct {
+	Id   int    `json:"id"`
+	Body string `json:"body"`
+}
+
+func (db *DB) CreateChirp(body string) (Chirp, error) {
+	dbStructure, err := db.loadDB()
+	if err != nil {
+		return Chirp{}, err
+	}
+
+	id := len(dbStructure.Chirps) + 1
+	newChirp := Chirp{
+		Id:   id,
+		Body: body,
+	}
+	dbStructure.Chirps[id] = newChirp
+
+	if writeErr := db.writeDB(dbStructure); writeErr != nil {
+		return Chirp{}, nil
+	}
+	return newChirp, nil
+}
+
+func (db *DB) GetChirps() ([]Chirp, error) {
+	dbStructure, err := db.loadDB()
+	if err != nil {
+		return nil, err
+	}
+
+	chirps := make([]Chirp, 0, len(dbStructure.Chirps))
+	for _, v := range dbStructure.Chirps {
+		chirps = append(chirps, v)
+	}
+
+	return chirps, nil
+}
+
+func (db *DB) GetChirp(id int) (Chirp, error) {
+	dbStructure, err := db.loadDB()
+	if err != nil {
+		return Chirp{}, err
+	}
+
+	chirp, ok := dbStructure.Chirps[id]
+	if !ok {
+		return Chirp{}, errors.New("Chirp not found")
+	}
+
+	return chirp, nil
+}
