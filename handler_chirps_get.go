166a9e5fc09ba4487877ec86ea3d5fa5e11dade1
ncfex <oguzhanguldibi@gmail.com>
2024-08-06 00:01:59 +0300

    Refactor Handlers

--- handler_chirps_get.go
+++ handler_chirps_get.go
@@ -1,17 +1,13 @@
 package main
 
 import (
-	"log"
 	"net/http"
 	"strconv"
-
-	"github.com/ncfex/chirpy/internal/database"
 )
 
 func (cfg *apiConfig) handlerGetChirps(rw http.ResponseWriter, r *http.Request) {
 	chirps, err := cfg.DB.GetChirps()
 	if err != nil {
-		log.Printf("Error getting chirps: %s", err)
 		respondWithError(rw, http.StatusInternalServerError, "Error getting chirps")
 		return
 	}
@@ -20,32 +16,17 @@ func (cfg *apiConfig) handlerGetChirps(rw http.ResponseWriter, r *http.Request) 
 }
 
 func (cfg *apiConfig) handlerGetChirpById(rw http.ResponseWriter, r *http.Request) {
-	chirps, err := cfg.DB.GetChirps()
-	if err != nil {
-		log.Printf("error getting  chirps")
-		respondWithError(rw, 500, "error getting  chirps")
-		return
-	}
-
 	chirpId, toIntErr := strconv.Atoi(r.PathValue("chirpId"))
 	if toIntErr != nil {
-		log.Printf("not valid ID")
-		respondWithError(rw, 500, "not valid ID")
+		respondWithError(rw, http.StatusInternalServerError, "not valid ID")
 		return
 	}
 
-	chirpToReturn := database.Chirp{}
-	for _, chirp := range chirps {
-		if chirp.Id == chirpId {
-			chirpToReturn = chirp
-		}
-	}
-
-	if chirpToReturn.Id == 0 {
-		log.Printf("chirp not found")
-		respondWithError(rw, 404, "chirp not found")
+	chirp, err := cfg.DB.GetChirp(chirpId)
+	if err != nil {
+		respondWithError(rw, http.StatusInternalServerError, "Error getting chirp")
 		return
 	}
 
-	respondWithJSON(rw, 200, chirpToReturn)
+	respondWithJSON(rw, http.StatusOK, chirp)
 }
